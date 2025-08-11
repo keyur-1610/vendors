6c5dc6ec0c565fc5484b4828c5cff13a899f297c
mann chopda <manchopda1508@gmail.com>
2025-08-11 22:41:51 +0530

    add backend url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vendors-0uvu.onrender.com/api",
 });
 
 // Automatically attach token to requests if available
